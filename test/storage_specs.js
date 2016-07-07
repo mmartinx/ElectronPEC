620a173258427ba99ae114d0ae9805b70c6a2456
Marshall Martin <mmartinx@gmail.com>
2016-07-07 15:31:58 -0400

    Testing git-plus

--- test/storage_specs.js
+++ test/storage_specs.js
@@ -8,48 +8,50 @@ const path = './data/test.json';
 let store = {};
 
 describe('Storage specs', () => {
-  beforeEach(() => {
-    store = new Storage('test');
-    store.destroy();
-  });
-
-  after(() => {
-    store = new Storage('test');
-    store.destroy();
-  });
-
-  describe('Initializing storage', () => {
-    it('Creates a file if one does not exist already', () => {
-      assert.throws(() => fs.accessSync(path));
-      store = new Storage('test');
-      assert.doesNotThrow(() => fs.accessSync(path));
+    beforeEach(() => {
+        store = new Storage('test');
+        store.destroy();
     });
-  });
 
-  describe('Saving data', () => {
-    it('Saves data', () => {
-      let collection = [
-        { Name: 'Jon'},
-        { Name: 'Ron'}
-      ];
-
-      store.saveCollection(collection);
-      let contents = fs.readFileSync(path, 'utf8');
-
-      assert.equal(JSON.stringify(collection), contents);
+    after(() => {
+        store = new Storage('test');
+        store.destroy();
     });
-  });
 
-  describe('Getting data', () => {
-    it('Storage with 2 items will return an array of length 2', () => {
-      let collection = [
-        { Name: 'Jon'},
-        { Name: 'Ron'}
-      ];
-
-      store.saveCollection(collection);
-      let people = store.getCollection();
-      assert.equal(collection.length, people.length);
+    describe('Initializing storage', () => {
+        it('Creates a file if one does not exist already', () => {
+            assert.throws(() => fs.accessSync(path));
+            store = new Storage('test');
+            assert.doesNotThrow(() => fs.accessSync(path));
+        });
     });
-  });
+
+    describe('Saving data', () => {
+        it('Saves data', () => {
+            let collection = [{
+                Name: 'Jon'
+            }, {
+                Name: 'Ron'
+            }];
+
+            store.saveCollection(collection);
+            let contents = fs.readFileSync(path, 'utf8');
+
+            assert.equal(JSON.stringify(collection), contents);
+        });
+    });
+
+    describe('Getting data', () => {
+        it('Storage with 2 items will return an array of length 2', () => {
+            let collection = [{
+                Name: 'Jon'
+            }, {
+                Name: 'Ron'
+            }];
+
+            store.saveCollection(collection);
+            let people = store.getCollection();
+            assert.equal(collection.length, people.length);
+        });
+    });
 });
